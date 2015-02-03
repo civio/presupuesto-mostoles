a1e1ebbb155661166d608cc476f4e84b990a69aa
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-02-03 17:56:47 +0100

    Content customization for Móstoles
    Closes https://github.com/dcabo/presupuesto-mostoles/issues/5
    Closes https://github.com/dcabo/presupuesto-mostoles/issues/6
    Closes https://github.com/dcabo/presupuesto-mostoles/issues/9

--- settings.py
+++ settings.py
@@ -7,9 +7,9 @@ MAIN_ENTITY_NAME = 'Móstoles'
 
 BUDGET_LOADER = 'MostolesBudgetLoader'
 
-FEATURED_PROGRAMMES = ['3211', '1710', '3320']
+FEATURED_PROGRAMMES = ['1321', '3347', '1711']
 
-OVERVIEW_INCOME_NODES = [['11', '113'], '42', ['11', '116'], '13', '34']
+OVERVIEW_INCOME_NODES = [['11', '113'], '42', '45', '29', '60']
 OVERVIEW_EXPENSE_NODES = ['23', '92', '13', '16', '15', '33']
 
 # Show an extra tab with institutional breakdown. Default: True.
